dbb197f363bc0380e56c1505185481cf5bb2a9c0
Mereles Lautaro <lautaro.jobs@hotmail.com>
2024-05-15 11:35:59 -0300

    Config Server Microservcios cities-hotels

---
+++ config-server/src/test/java/com/lautadev/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lautadev.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
